f41e6357c777947b01b7623cae2b5aa16cb741db
paurushofficial <47181643+paurushofficial@users.noreply.github.com>
2020-02-16 19:47:15 +0530

    Add files via upload

---
+++ E-InvoiceGenerator.py
@@ -0,0 +1,226 @@
+from tkinter import *
+import tkinter.ttk as ttk
+import tkinter.messagebox as tkMessageBox
+import time
+import pdfgen
+from reportlab.pdfgen import canvas
+import webbrowser,os
+
+try:
+	os.mkdir("C:\\InvoiceGenerator")
+except:
+	pass
+class header:
+	def __init__(self,CustomerName,CustomerContact):
+		self.InvoiceNumber=time.time()
+		self.CustomerName=CustomerName
+		self.CustomerContact=CustomerContact
+		timedate=time.asctime()
+		self.date=timedate[4:8]+timedate[8:10]+", "+timedate[20:24]+"."
+		self.time=" "+timedate[11:20]
+class product:
+	def __init__(self,name,quantity,rate,tax,discount):
+		self.name=name
+		self.quantity=quantity
+		self.rate=rate
+		self.tax=tax
+		self.total=(quantity*rate)-discount
+		self.discount=discount
+root = Tk()
+root.title("E-INVOICE GENERATOR")
+width = 700
+height = 400
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+x = (screen_width/2) - (width/2)
+y = (screen_height/2) - (height/2)
+root.geometry("%dx%d+%d+%d" % (width, height, x, y))
+root.resizable(0, 0)
+root.config(bg="#6666ff")
+
+PRODUCTNAME = StringVar()
+QUANTITY = IntVar()
+RATE = IntVar()
+TAX = IntVar()
+TOTAL = IntVar()
+DISCOUNT=IntVar()
+CustomerName=StringVar()
+CustomerContact=StringVar()
+
+
+Products=[]
+
+def printinvoice():
+    head=header(CustomerName.get(),CustomerContact.get())
+    pdf= canvas.Canvas("C:\\InvoiceGenerator\\"+str(int(head.InvoiceNumber))+".pdf")
+    pdfgen.header(head,pdf)
+    pdfgen.middle(pdf)
+    ycooridinate=650
+    x=1
+
+    for item in Products:
+        currproduct=product(item[0],item[1],item[2],item[5],item[3])
+        pdf.drawString(35,ycooridinate,str(x))
+        x=x+1
+        pdf.setFont("Courier-Bold",9)
+        ycooridinate=pdfgen.additem(currproduct,pdf,ycooridinate)
+    pdf.setFont("Courier-Bold",11)
+    pdfgen.footer(pdf,Products)
+    pdf.save()
+    webbrowser.open("C:\\InvoiceGenerator\\"+str(int(head.InvoiceNumber))+".pdf")
+	
+
+
+def SubmitData():
+    product=PRODUCTNAME.get()
+    quantity=QUANTITY.get()
+    rate=RATE.get()
+
+    dis=float((quantity*rate)*(DISCOUNT.get()/100))
+    total=float(QUANTITY.get()*RATE.get()-dis)
+    tax=float(TAX.get()*0.01*total)
+
+    Products.append((product,quantity,rate,dis,total,tax))
+    tree.delete(*tree.get_children())
+    for data in (Products):
+        tree.insert('', 'end', values=(data))
+    PRODUCTNAME.set("")
+    QUANTITY.set(1)
+    RATE.set(0)
+
+
+def DeleteData():
+    if not tree.selection():
+       result = tkMessageBox.showwarning('', 'Please Select Something First!', icon="warning")
+    else:
+        result = tkMessageBox.askquestion('', 'Are you sure you want to delete this item?', icon="warning")
+        if result == 'yes':
+            curItem = tree.focus()
+            contents =(tree.item(curItem))
+            selecteditem = contents['values']
+            tree.delete(curItem)
+            print(selecteditem)
+            for item in range(len(Products)):
+            	if((Products[item])[0]==selecteditem[0]):
+            		break
+            del(Products[item])
+            print(Products)
+
+def AddNewWindow():
+    global NewWindow
+    PRODUCTNAME.set("")
+    QUANTITY.set("")
+    RATE.set("")
+    NewWindow = Toplevel()
+    NewWindow.title("E-INVOICE GENERATOR")
+    width = 400
+    height = 330
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    x = ((screen_width/2) - 455) - (width/2)
+    y = ((screen_height/2) + 20) - (height/2)
+    NewWindow.resizable(0, 0)
+    NewWindow.geometry("%dx%d+%d+%d" % (width, height, x, y))
+    if 'UpdateWindow' in globals():
+        UpdateWindow.destroy()
+
+    
+    #===================FRAMES==============================
+    FormTitle = Frame(NewWindow)
+    FormTitle.pack(side=TOP)
+    Form = Frame(NewWindow)
+    Form.pack(side=TOP, pady=10)
+ 
+    #===================LABELS==============================
+    lbl_title = Label(FormTitle, text="Add Item", font=('arial', 16), bg="#66ff66",  width = 300)
+    lbl_title.pack(fill=X)
+    lbl_productname = Label(Form, text="Product name", font=('arial', 14), bd=5)
+    lbl_productname.grid(row=0, sticky=W)
+    lbl_quantity = Label(Form, text="Quantity", font=('arial', 14), bd=5)
+    lbl_quantity.grid(row=1, sticky=W)
+    lbl_rate = Label(Form, text="Rate", font=('arial', 14), bd=5)
+    lbl_rate.grid(row=2, sticky=W)
+    taxlabel=Label(Form,text="Tax(%)",font=('arial',14),bd=5)
+    taxlabel.grid(row=3,sticky=W)
+    discountLabel= Label(Form,text="Discount(%)",font=('arial',14),bd=5)
+    discountLabel.grid(row=4,sticky=W)
+    nameLabel=Label(Form,text="Customer Name:",font=('arial',14),bd=5)
+    nameLabel.grid(row=5,sticky=W)
+    nameLabel=Label(Form,text="Customer Contact:",font=('arial',14),bd=5)
+    nameLabel.grid(row=6,sticky=W)
+
+
+
+    #===================ENTRY===============================
+    Product = Entry(Form, textvariable=PRODUCTNAME, font=('arial', 14))
+    Product.grid(row=0, column=1)
+    Quantity = Entry(Form, textvariable=QUANTITY, font=('arial', 14))
+    Quantity.grid(row=1, column=1)
+    Rate= Entry(Form, textvariable=RATE,  font=('arial', 14))
+    Rate.grid(row=2, column=1)
+    Product.focus()
+    Tax=Entry(Form,textvariable=TAX, font=('arial',14))
+    Tax.grid(row=3,column=1)
+    discountEntry=Entry(Form,textvariable=DISCOUNT, font=('arial',14))
+    discountEntry.grid(row=4,column=1)
+    nameEntry=Entry(Form,textvariable=CustomerName,font=('arial',14))
+    nameEntry.grid(row=5, column=1)
+    nameEntry=Entry(Form,textvariable=CustomerContact,font=('arial',14))
+    nameEntry.grid(row=6, column=1)
+    RATE.set(0)
+    TAX.set(0)
+    QUANTITY.set(1)
+
+
+
+    btn_addcon = Button(Form, text="Add Item to Cart", width=50, command=SubmitData)
+    btn_addcon.grid(row=7, columnspan=2, pady=10)
+
+#============================FRAMES======================================
+Top = Frame(root, width=500, bd=1, relief=SOLID)
+Top.pack(side=TOP)
+Mid = Frame(root, width=500,  bg="#6666ff")
+Mid.pack(side=TOP)
+MidLeft = Frame(Mid, width=100)
+MidLeft.pack(side=LEFT, pady=10)
+MidLeftPadding = Frame(Mid, width=370, bg="#6666ff")
+MidLeftPadding.pack(side=LEFT)
+MidRight = Frame(Mid, width=100)
+MidRight.pack(side=RIGHT, pady=10)
+TableMargin = Frame(root, width=500)
+TableMargin.pack(side=TOP)
+#============================LABELS======================================
+lbl_title = Label(Top, text="Prem Mobile", font=('arial', 16), width=500)
+lbl_title.pack(fill=X)
+
+#============================BUTTONS=====================================
+btn_add = Button(MidLeft, text="Add New Item", bg="#66ff66", command=AddNewWindow)
+btn_add.pack()
+btn_delete = Button(MidRight, text="Remove Selected Item", bg="red", command=DeleteData)
+btn_delete.pack(side=RIGHT)
+btn_about= Button(TableMargin,text="About Developer  \t\t\t\t\t\t\t\t\t\t\t\t\t",command=pdfgen.About)
+btn_about.pack(side=BOTTOM)
+btn_print= Button(TableMargin,text="Print Invoice",bg="lightgreen",command=printinvoice)
+btn_print.pack(side=BOTTOM)
+
+scrollbarx = Scrollbar(TableMargin, orient=HORIZONTAL)
+scrollbary = Scrollbar(TableMargin, orient=VERTICAL)
+tree = ttk.Treeview(TableMargin, columns=("Product Name", "Quantity", "Rate","Discount", "Total", "Tax"), height=400, selectmode="extended", yscrollcommand=scrollbary.set, xscrollcommand=scrollbarx.set)
+scrollbary.config(command=tree.yview)
+scrollbary.pack(side=RIGHT, fill=Y)
+scrollbarx.config(command=tree.xview)
+scrollbarx.pack(side=BOTTOM, fill=X)
+tree.heading('Product Name', text="Product Name", anchor=W)
+tree.heading('Quantity', text="Quantity", anchor=W)
+tree.heading('Rate', text="Rate", anchor=W)
+tree.heading('Discount',text="Discount",anchor=W)
+tree.heading('Total', text="Total", anchor=W)
+tree.heading('Tax', text="Tax", anchor=W)
+tree.column('#0',width=0)
+tree.column('#1', stretch=NO, minwidth=0, width=250)
+tree.column('#2', stretch=NO, minwidth=0, width=70)
+tree.column('#3', stretch=NO, minwidth=0, width=90)
+tree.column('#4', stretch=NO, minwidth=0, width=80)
+tree.column('#5', stretch=NO, minwidth=0, width=120)
+tree.pack()
+root.mainloop()
---
+++ pdfgen.py
@@ -0,0 +1,125 @@
+import qrcode,webbrowser
+from reportlab.pdfgen import canvas
+def getTotal(list):
+  total=0
+  for data in list:
+    total=total+data[4]
+  return total
+
+def getTotalTax(list):
+  totalTax=0
+  for data in list:
+    totalTax=totalTax+data[5]
+  return totalTax
+
+def getTotalDis(list):
+  totalDis=0
+  for data in list:
+    totalDis=totalDis+data[3]
+  return totalDis
+
+def rightalingn(pdf,string,left,right,ycoordinate):
+  length=len(string)
+  totalLength=(right-left)/7
+  print(totalLength)
+  spaces=int(totalLength-length)
+  print(spaces)
+  pdf.drawString(right,ycoordinate," "*spaces)
+  left=left+(7*spaces)
+  pdf.drawString(left,ycoordinate,string)
+
+def header(header,pdf):
+  pdf.setTitle(header.date+"Invoice")
+  # logo="logo.jpeg"
+  # pdf.drawInlineImage(logo,190,253)
+
+  pdf.line(30,815,350,815)
+  pdf.setFont("Courier-Bold",20)
+  pdf.drawString(30,800,"Prem Mobile")
+  pdf.setFont("Courier-Bold",11)
+  pdf.drawString(30,785,"Barlai Mod - Badagaon,")
+  pdf.drawString(30,770,"Mau, Uttar Pradesh, India,")
+  pdf.drawString(30,755,"275101.  Phone: 9450037365")
+
+  pdf.line(30,753,350,753)
+
+  pdf.drawString(30,735,"Invoice Number: "+ str(int(header.InvoiceNumber)))
+  pdf.drawString(30,720,"Customer Name: "+ str(header.CustomerName))
+  pdf.drawString(30,705,"Contact Number: "+ str(header.CustomerContact))
+  pdf.drawString(30,690,"Date: "+ str(header.date))
+  
+  qr = qrcode.QRCode(
+    version=1,
+    box_size=2,
+    border=2
+  )
+
+  data = 'InvoiceNumber:'+str(int(header.InvoiceNumber))+'\nTime:'+(header.date+"--"+header.time)+"\nGoogle Map: https://goo.gl/maps/DV59uHs2eoYpuvsC9\nTerms & Conditions:\n1:Sold items will not be returned or changed.\n2:Every dispute will settled in Mau Court.\n3:Battery, Charger, Mobile warranty is provided by service center.\nTHANK YOU"
+  qr.add_data(data)
+  qr.make(fit=True)
+  img = qr.make_image(fill='black', back_color='white')
+
+  pdf.drawInlineImage(img,400,685)
+  
+
+def middle(pdf):
+  pdf.line(30,677,550,677)
+  
+  pdf.drawString(30,668,"Sr.No.")
+  pdf.drawString(75,668,"Product Name")
+  pdf.drawString(200,668,"Quantity")
+  pdf.drawString(260,668,"Rate")
+  pdf.drawString(320,668,"Discount")
+  pdf.drawString(400,668,"Total")
+  pdf.drawString(490,668,"Tax")
+  
+  pdf.line(30,662,550,662)
+  pdf.line(73,677,73,150)
+  pdf.line(198,677,198,150)
+  pdf.line(258,677,258,150)
+  pdf.line(318,677,318,150)
+  pdf.line(398,677,398,150)
+  pdf.line(488,677,488,150)
+  pdf.line(30,150,550,150)
+
+def additem(product,pdf,ycoordinate):
+  while(len(product.name)>18):
+    pdf.drawString(75,ycoordinate,product.name[:18]+"-")
+    product.name=product.name[18:]
+    ycoordinate=ycoordinate-15
+  
+  pdf.drawString(75,ycoordinate,product.name)
+  pdf.drawString(200,ycoordinate,str(product.quantity))
+  rightalingn(pdf,"%.2f" %product.rate,260,316,ycoordinate)
+  print(product.rate)
+  rightalingn(pdf,"%.2f" %product.discount,320,398,ycoordinate)
+  rightalingn(pdf,"%.2f" %product.total,400,488,ycoordinate)
+  rightalingn(pdf,"%.2f" %product.tax,490,552,ycoordinate)
+  return (ycoordinate-15)
+
+def footer(pdf,list):
+  pdf.drawString(30,135,"Total Discount:")
+  rightalingn(pdf,"-"+"%.2f" %getTotalDis(list)+" INR",393,488,135)
+  pdf.drawString(30,120,"Gross Total(Discount Included):")
+  rightalingn(pdf,"%.2f" %getTotal(list)+" INR",400,488,120)
+  pdf.drawString(30,105,"Tax:")
+  rightalingn(pdf,"+"+"%.2f" %getTotalTax(list)+" INR",393,488,105)
+  pdf.line(30,100,550,100)
+  pdf.drawString(30,90,"Grand Total: ")
+  rightalingn(pdf,"%.2f" %(getTotal(list)+getTotalTax(list))+" INR",400,488,90)
+  pdf.drawString(400,50,"Authorized Signatory")
+  pdf.setFont("Courier-Bold",7)
+  pdf.drawString(15,15,"Scan QR code for Applied Terms & Conditions.")
+
+def About():
+  pdf=canvas.Canvas("C:\\InvoiceGenerator\\about.pdf")
+  pdf.setFont("Courier-Bold",20)
+  pdf.drawString(40,700,"Paurush Kumar Gupta")
+  pdf.setFont("Courier-Bold",15)
+  pdf.drawString(40,650,"Student, ")
+  pdf.drawString(40,630,"Computer Science & Engineering,")
+  pdf.drawString(40,610,"Noida Institute of Engineering & Technology,")
+  pdf.drawString(40,590,"Greater Noida, 201310.")
+  pdf.drawString(40,550,"Contact: gpaurush39@gmail.com")
+  pdf.save()
+  webbrowser.open("C:\\InvoiceGenerator\\about.pdf")
